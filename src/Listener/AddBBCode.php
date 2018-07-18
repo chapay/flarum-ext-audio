b1982bcab8f9985c945a11ca926043c5fa9e6806
breathingboard <41351317+breathingboard@users.noreply.github.com>
2018-07-18 19:58:29 -0400

    Remove a comma to prevent 500 error

--- src/Listener/AddBBCode.php
+++ src/Listener/AddBBCode.php
@@ -30,19 +30,19 @@ class AddBBCode
     {
         $event->configurator->BBCodes->addCustom(
             '[AUDIO]{URL;useContent}[/AUDIO]',
-            '<audio src="{URL}" preload="none" controls>Sorry, your browser does not support this audio player or the file type.</audio>'
+            '<audio src="{URL}" preload="none" controls>Sorry your browser does not support this audio player or the file type.</audio>'
         );
         $event->configurator->BBCodes->addCustom(
-            '[AUDIO]mp3={MP3;useContent}ogg={OGG;useContent}wav={WAV;useContent}[/AUDIO]',
-            '<audio preload="none" controls><source src="{MP3}" type="audio/mpeg"><source src="{OGG}" type="audio/ogg"><source src="{WAV}" type="audio/wav">Sorry, your browser does not support this audio player or the file type.</audio>'
+            '[AUDIO]mp3={URL1;useContent}ogg={URL2;useContent}wav={URL3;useContent}[/AUDIO]',
+            '<audio preload="none" controls><source src="{URL1}" type="audio/mpeg"><source src="{URL2}" type="audio/ogg"><source src="{URL3}" type="audio/wav">Sorry your browser does not support this audio player or the file type.</audio>'
         );
         $event->configurator->BBCodes->addCustom(
-            '[AUDIO]mp3={MP3;useContent}ogg={OGG;useContent}[/AUDIO]',
-            '<audio preload="none" controls><source src="{MP3}" type="audio/mpeg"><source src="{OGG}" type="audio/ogg">Sorry, your browser does not support this audio player or the file type.</audio>'
+            '[AUDIO]mp3={URL4;useContent}ogg={URL5;useContent}[/AUDIO]',
+            '<audio preload="none" controls><source src="{URL4}" type="audio/mpeg"><source src="{URL5}" type="audio/ogg">Sorry your browser does not support this audio player or the file type.</audio>'
         );
         $event->configurator->BBCodes->addCustom(
-            '[AUDIO]mp3={MP3;useContent}wav={WAV;useContent}[/AUDIO]',
-            '<audio preload="none" controls><source src="{MP3}" type="audio/mpeg"><source src="{WAV}" type="audio/wav">Sorry, your browser does not support this audio player or the file type.</audio>'
+            '[AUDIO]mp3={URL6;useContent}wav={URL7;useContent}[/AUDIO]',
+            '<audio preload="none" controls><source src="{URL6}" type="audio/mpeg"><source src="{URL7}" type="audio/wav">Sorry your browser does not support this audio player or the file type.</audio>'
         );
     }
 }
