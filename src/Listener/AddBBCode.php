9ef606afa1cf298b8c62de0a038a82c05e9d38de
breathingboard <41351317+breathingboard@users.noreply.github.com>
2018-07-19 23:03:21 -0400

    Revert!
    Simply put something back to the way it was originally.

--- src/Listener/AddBBCode.php
+++ src/Listener/AddBBCode.php
@@ -32,17 +32,5 @@ class AddBBCode
             '[AUDIO]{URL;useContent}[/AUDIO]',
             '<audio src="{URL}" preload="none" controls>Sorry your browser does not support this audio player or the file type.</audio>'
         );
-        $event->configurator->BBCodes->addCustom(
-            '[AUDIO]mp3={URL1;useContent}ogg={URL2;useContent}wav={URL3;useContent}[/AUDIO]',
-            '<audio preload="none" controls><source src="{URL1}" type="audio/mpeg"><source src="{URL2}" type="audio/ogg"><source src="{URL3}" type="audio/wav">Sorry your browser does not support this audio player or the file type.</audio>'
-        );
-        $event->configurator->BBCodes->addCustom(
-            '[AUDIO]mp3={URL4;useContent}ogg={URL5;useContent}[/AUDIO]',
-            '<audio preload="none" controls><source src="{URL4}" type="audio/mpeg"><source src="{URL5}" type="audio/ogg">Sorry your browser does not support this audio player or the file type.</audio>'
-        );
-        $event->configurator->BBCodes->addCustom(
-            '[AUDIO]mp3={URL6;useContent}wav={URL7;useContent}[/AUDIO]',
-            '<audio preload="none" controls><source src="{URL6}" type="audio/mpeg"><source src="{URL7}" type="audio/wav">Sorry your browser does not support this audio player or the file type.</audio>'
-        );
     }
 }
